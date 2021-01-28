4cdf90547675665b9c26d34d0464f30826c98954
Eman Megahed <50622434+eman1233@users.noreply.github.com>
2021-01-28 10:33:36 +0200

    Add files via upload

---
+++ Std_Types.h
@@ -0,0 +1,20 @@
+/*
+ * Std_Types.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: inter
+ */
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+#include "Platform_Types.h"
+#include "Compiler.h"
+
+typedef enum
+{
+	E_NOT_OK = 0,
+	E_OK
+}Std_ReturnType;
+
+#endif /* STD_TYPES_H_ */
