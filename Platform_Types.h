3448f480f578ecda087ed6c2f6e4c9f018627b8e
Eman Megahed <50622434+eman1233@users.noreply.github.com>
2021-01-28 10:34:32 +0200

    Add files via upload

---
+++ Platform_Types.h
@@ -0,0 +1,33 @@
+/*
+ * Platform_Types.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: inter
+ */
+
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+
+typedef unsigned char      u8;
+
+typedef signed char        s8;
+
+typedef unsigned short int u16;
+
+typedef signed short int   s16;
+
+typedef unsigned long int  u32;
+
+typedef signed long int    s32;
+
+typedef float              f32;
+
+typedef long double        f128;
+
+#define STD_OK 1
+
+#define STD_NOT_OK 0
+
+#define NULL ((void*)0)
+
+#endif /* PLATFORM_TYPES_H_ */
